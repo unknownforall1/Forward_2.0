efc34beece2d6b7b95dbf9d04e2e8a4d56642cb0
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-27 23:51:01 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -1,6 +1,4 @@
 import os
-import string
-import requests 
 class temp(object):
     BANNED_USERS = []
     BANNED_CHATS = []
@@ -177,7 +175,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
         try:
             current = temp.CURRENT
             temp.CANCEL = False
-            async for message in bot.iter_messages(chat, lst_msg_id, temp.CURRENT):
+            async for message in bot.get_messages(chat, lst_msg_id, temp.CURRENT):
                 if temp.CANCEL:
                     await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
                     break

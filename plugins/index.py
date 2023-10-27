bb2bd2fae8473aec9ffa955e17c5cdebf36039d8
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-27 20:52:17 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -29,7 +29,7 @@ async def run(bot, message):
         return
     while True:
         try:
-            chat = await bot.ask(text = "To Index a channel you may send me the channel invite link, so that I can join channel and index the files.\n\nIt should be something like <code>https://t.me/xxxxxx</code> or <code>https://t.me/joinchat/xxxxxx</code>", chat_id = message.from_user.id, filters=filters.text, timeout=30)
+            chat = await bot.ask(chat_id = message.from_user.id, text = "To Index a channel you may send me the channel invite link, so that I can join channel and index the files.\n\nIt should be something like <code>https://t.me/xxxxxx</code> or <code>https://t.me/joinchat/xxxxxx</code>", filters=filters.text, timeout=30)
             channel=int(chat.text)
         except TimeoutError:
             await bot.send_message(message.from_user.id, "Error!!\n\nRequest timed out.\nRestart by using /index")

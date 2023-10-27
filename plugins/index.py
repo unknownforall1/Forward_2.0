00e7c5532a40e991062ba9305e6b958fed38e74c
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-28 01:14:39 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -193,9 +193,3 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                         await msg.edit(f'Error: {e}')
                     else:
                         await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
-
-
-
-
-
-
